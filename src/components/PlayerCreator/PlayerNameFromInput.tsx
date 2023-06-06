387fcda68ac5749e134965fa57729445202ddb3d
Mati <107854399+deretvir@users.noreply.github.com>
2023-06-06 12:22:02 +0000

    Implemented correct compontent idea to PlayerNameFromInput

--- src/components/PlayerCreator/PlayerNameFromInput.tsx
+++ src/components/PlayerCreator/PlayerNameFromInput.tsx
@@ -1,6 +1,26 @@
 import React, { useState } from "react";
 
-const dimension = 12;
+
+function Input({ handlePlayerName, handleEnterKey }){
+  return(
+     <input
+          type="text"
+          placeholder="Player name"
+          className="flex-grow rounded-l-md outline-none py-2 px-5"
+          onChange={handlePlayerName}
+          onKeyDown={handleEnterKey}
+        />
+  )}
+  
+  function InputBtn ({displayName}){
+    return(
+    <button
+          className={`bg-orange-400 w-12 rounded-r-md text-white`}
+          onClick={displayName}> +
+        </button>
+    )}
+
+
 
 function PlayerNameFromInput({ addPlayer }) {
   const [playerName, setPlayerName] = useState("");
@@ -25,37 +45,15 @@ function PlayerNameFromInput({ addPlayer }) {
     <div className="flex flex-col justify-center items-start">
       <p className="text-l text-white">Add player</p>
     
-      <div className={`flex w-full h-${dimension}`}>
+      <div className={`flex w-full h-12`}>
          <Input handlePlayerName={handlePlayerName} handleEnterKey={handleEnterKey} />
-         
+         <InputBtn displayName={displayName} />
       </div>
       <p className="text-l text-white">
         Press "Enter" to add a player or press the button
       </p>
     </div>
   );
-}
-
-
-function Input({ handlePlayerName, handleEnterKey }){
-  return(
-    <>
-     <input
-          type="text"
-          placeholder="Player name"
-          className="flex-grow rounded-l-md outline-none py-2 px-5"
-          onChange={handlePlayerName}
-          onKeyDown={handleEnterKey}
-        />
-    </>
-  )
-  
-  function Btn ({displayName}){
-    <button
-          className={`bg-orange-400 w-${dimension} rounded-r-md text-white`}
-          onClick={displayName}> +
-        </button>
-  }
 }
 
 export default PlayerNameFromInput;
