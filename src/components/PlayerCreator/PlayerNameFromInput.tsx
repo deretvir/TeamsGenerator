09430d0284f25be6b1d8e8ccb80a8aad19afd9ca
Mati <107854399+deretvir@users.noreply.github.com>
2023-06-06 08:44:49 +0000

    NumberTeamsOrPlayers compontent - optimize component Idea (creat components  from buttons to avoid repeats)

--- src/components/PlayerCreator/PlayerNameFromInput.tsx
+++ src/components/PlayerCreator/PlayerNameFromInput.tsx
@@ -24,27 +24,38 @@ function PlayerNameFromInput({ addPlayer }) {
   return (
     <div className="flex flex-col justify-center items-start">
       <p className="text-l text-white">Add player</p>
-
+    
       <div className={`flex w-full h-${dimension}`}>
-        <input
-          type="text"
-          placeholder="Player name"
-          className="flex-grow rounded-l-md outline-none py-2 px-5"
-          onChange={handlePlayerName}
-          onKeyDown={handleEnterKey}
-        />
-        <button
-          className={`bg-orange-400 w-${dimension} rounded-r-md text-white`}
-          onClick={displayName}
-        >
-          +
-        </button>
+         <Input handlePlayerName={handlePlayerName} handleEnterKey={handleEnterKey} />
+         
       </div>
       <p className="text-l text-white">
         Press "Enter" to add a player or press the button
       </p>
     </div>
   );
+}
+
+
+function Input({ handlePlayerName, handleEnterKey }){
+  return(
+    <>
+     <input
+          type="text"
+          placeholder="Player name"
+          className="flex-grow rounded-l-md outline-none py-2 px-5"
+          onChange={handlePlayerName}
+          onKeyDown={handleEnterKey}
+        />
+    </>
+  )
+  
+  function Btn ({displayName}){
+    <button
+          className={`bg-orange-400 w-${dimension} rounded-r-md text-white`}
+          onClick={displayName}> +
+        </button>
+  }
 }
 
 export default PlayerNameFromInput;
--- src/components/TeamGenerator/NumberTeamsOrPlayers.tsx
+++ src/components/TeamGenerator/NumberTeamsOrPlayers.tsx
@@ -1,5 +1,14 @@
 import React, { useState } from "react";
 
+function NumberChangeBtn( {addCounter,sign, radius}){
+  return ( <button
+          className={`bg-orange-400 w-14 ${radius} text-white h-full`}
+          onClick={addCounter}
+        >
+          {sign}
+        </button>)
+}
+
 const NumberTeamsOrPlayers = () => {
   const [isTeamsActive, setIsTeamsActive] = useState(true);
   const [counter, setCounter] = useState(1);
@@ -40,21 +49,11 @@ const NumberTeamsOrPlayers = () => {
       </div>
 
       <div className="flex flex-row h-12 w-full justify-between items-start py-0 m-0 border border-stone-700 rounded-md border-opacity-40">
-        <div
-          className="w-14 h-full bg-orange-400 rounded-l-md flex justify-center items-center font-bold text-white"
-          onClick={minusCounter}
-        >
-          -
-        </div>
+     <NumberChangeBtn addCounter={minusCounter} sign="-" radius="rounded-l-md"/>
         <div className="bg-stone-[rgb(128, 128, 129)]] h-full  w-full flex justify-center items-center px-8">
           {counter}
         </div>
-        <button
-          className="bg-orange-400 w-14 rounded-r-md text-white h-full"
-          onClick={addCounter}
-        >
-          +
-        </button>
+       <NumberChangeBtn addCounter={addCounter} sign="+" radius="rounded-r-md"/>
       </div>
     </>
   );
